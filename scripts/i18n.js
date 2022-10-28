97757acfdf715baa76a0bd0a952790c8d7a5c018
luke <luke@lukehanslarsen.com>
2022-10-28 11:57:43 -0400

    i18n

---
+++ scripts/i18n.js
@@ -0,0 +1,25 @@
+if (networkEnabled) {
+    var translate = function(i18nLangs){
+        if (!i18nLangs) {return;}
+
+        document.querySelectorAll("[data-i18n]").forEach(function(element) {
+            if (! i18nLangs[element.dataset.i18n]) {
+                return;
+            }
+
+            if (element.dataset.i18n_target) {
+                element[element.dataset.i18n_target] = i18nLangs[element.dataset.i18n];
+            } else {
+                switch(element.tagName.toLowerCase()) {
+                    case "input":
+                    case "textarea":
+                        element.placeholder = i18nLangs[element.dataset.i18n];
+                    default:
+                        element.innerHTML = i18nLangs[element.dataset.i18n];
+                }
+            }
+        });
+    }
+}else{
+
+}
