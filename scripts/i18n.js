525382d864e07aeaedffa159a597c1ffcb7bd79e
luke <luke@lukehanslarsen.com>
2022-10-28 12:00:15 -0400

    Removed old check

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -1,25 +1,21 @@
-if (networkEnabled) {
-    var translate = function(i18nLangs){
-        if (!i18nLangs) {return;}
+var translate = function(i18nLangs){
+    if (!i18nLangs) {return;}
 
-        document.querySelectorAll("[data-i18n]").forEach(function(element) {
-            if (! i18nLangs[element.dataset.i18n]) {
-                return;
+    document.querySelectorAll("[data-i18n]").forEach(function(element) {
+        if (! i18nLangs[element.dataset.i18n]) {
+            return;
+        }
+
+        if (element.dataset.i18n_target) {
+            element[element.dataset.i18n_target] = i18nLangs[element.dataset.i18n];
+        } else {
+            switch(element.tagName.toLowerCase()) {
+                case "input":
+                case "textarea":
+                    element.placeholder = i18nLangs[element.dataset.i18n];
+                default:
+                    element.innerHTML = i18nLangs[element.dataset.i18n];
             }
-
-            if (element.dataset.i18n_target) {
-                element[element.dataset.i18n_target] = i18nLangs[element.dataset.i18n];
-            } else {
-                switch(element.tagName.toLowerCase()) {
-                    case "input":
-                    case "textarea":
-                        element.placeholder = i18nLangs[element.dataset.i18n];
-                    default:
-                        element.innerHTML = i18nLangs[element.dataset.i18n];
-                }
-            }
-        });
-    }
-}else{
-
+        }
+    });
 }
