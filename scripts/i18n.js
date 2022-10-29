a7de2bfdddd3b625b8831d2739b785f3cf5b5cfb
JSKitty <jskitty@protonmail.com>
2022-10-29 02:52:24 +0100

    [Misc] Cleanup + fixes
    Some code cleanup, styling, and some functionality to better support languages that may have multiple slightly-different 'subsets' but don't necessarily need their own entire translation file, f.e: en-GB, en-US and the rest.

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -1,15 +1,13 @@
-var translate = function(i18nLangs){
-    if (!i18nLangs) {return;}
+var translate = function(i18nLangs) {
+    if (!i18nLangs) return;
 
     document.querySelectorAll("[data-i18n]").forEach(function(element) {
-        if (! i18nLangs[element.dataset.i18n]) {
-            return;
-        }
+        if (!i18nLangs[element.dataset.i18n]) return;
 
         if (element.dataset.i18n_target) {
             element[element.dataset.i18n_target] = i18nLangs[element.dataset.i18n];
         } else {
-            switch(element.tagName.toLowerCase()) {
+            switch (element.tagName.toLowerCase()) {
                 case "input":
                 case "textarea":
                     element.placeholder = i18nLangs[element.dataset.i18n];
