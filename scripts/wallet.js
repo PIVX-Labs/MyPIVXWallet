5364630ff9c819c641f9fc10ee867f5ae780cfbf
JSKitty <jskitty@protonmail.com>
2022-10-30 16:35:03 +0000

    [Ledger] Add more error conversions
    Porting a couple more known 'unknown error' translations

--- scripts/wallet.js
+++ scripts/wallet.js
@@ -104,10 +104,13 @@ class LegacyMasterKey extends MasterKey {
 const LEDGER_ERRS = new Map([
   // Ledger error code <--> User-friendly string
   [25870, "Open the PIVX app on your device"],
+  [25873, "Open the PIVX app on your device"],
   [57408, "Navigate to the PIVX app on your device"],
+  [27157, "Wrong app! Open the PIVX app on your device"],
   [27266, "Wrong app! Open the PIVX app on your device"],
   [27904, "Wrong app! Open the PIVX app on your device"],
-  [27010, "Unlock your Ledger, then try again!"]
+  [27010, "Unlock your Ledger, then try again!"],
+  [27404, "Unlock your Ledger, then try again!"]
 ]);
 
 // Construct a full BIP44 pubkey derivation path from it's parts
