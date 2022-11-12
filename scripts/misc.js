e6b4eb64326500fd4a509fea80bbf65822a3f820
Duddino <duddinogatto@pm.me>
2022-11-12 14:22:43 +0100

    Cleanup + better css

--- scripts/misc.js
+++ scripts/misc.js
@@ -216,3 +216,9 @@ async function hash(byteArray) {
       .join('');
     return hashHex;
   }
+
+function sanitizeHTML(text) {
+  const element = document.createElement('div');
+  element.innerText = text;
+  return element.innerHTML;
+}
