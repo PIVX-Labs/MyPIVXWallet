83f1d8b078db07812733dd73b5afbe316fd24f67
Duddino <duddinogatto@pm.me>
2022-12-31 03:29:55 +0100

    Added npm run build, cleaned webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,42 +4,40 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
-    entry: './scripts/index.js',
-    /*output: {
-	filename: 'main.js',
-	path: path.resolve(__dirname, 'dist'),
-    },*/
-    output: {
-	path: path.resolve(__dirname, './dist'),
-	filename: './mpw.js',
-	library: 'MPW',
-	libraryTarget: 'var',
-    },
-    mode: "development",
-    module: {
-	rules: [
-	    {
-		test: /\.css$/i,
-		use: ['style-loader', 'css-loader']
-	    },
-	],
-	
-    },
-    devtool: "source-map",
-    plugins: [
-	new CleanWebpackPlugin({
-            verbose: true
-	}),
-	new HtmlWebpackPlugin({ template: './index.html', filename: 'index.html', inject: "head", scriptLoading: "blocking", }),
-	new NodePolyfillPlugin(),
-	new webpack.ProvidePlugin({
-            $: "jquery",
-          jQuery: "jquery",
-	  'window.jQuery': 'jquery',
-	  tether: 'tether',
-	  Tether: 'tether',
-	  'window.Tether': 'tether',
-	}),
-	//new webpack.IgnorePlugin(/^\.\/wordlists\/(?!english)/, /bip39\/src$/),
+  entry: './scripts/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: './mpw.js',
+    library: 'MPW',
+    libraryTarget: 'var',
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+	test: /\.css$/i,
+	use: ['style-loader', 'css-loader']
+      },
     ],
+  },
+  devtool: "source-map",
+  plugins: [
+    //new CleanWebpackPlugin({
+    //    verbose: true
+    //}),
+    new HtmlWebpackPlugin({ template: './index.html', filename: 'index.html', inject: "head", scriptLoading: "blocking", }),
+    new NodePolyfillPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      'window.jQuery': 'jquery',
+      tether: 'tether',
+      Tether: 'tether',
+      'window.Tether': 'tether',
+    }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/wordlists\/(?!english)/,
+      contextRegExp: /bip39\/src$/
+    }),
+  ],
 };
