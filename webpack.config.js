a68e46d81c21a979e11271295502e3435c875d20
Duddino Gatto <duddinogatto@pm.me>
2023-01-01 03:53:12 +0100

    Cleaned up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,50 +5,50 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
-  entry: './scripts/index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: './mpw.js',
-    library: 'MPW',
-    libraryTarget: 'var',
-  },
-  mode: "development",
-  module: {
-    rules: [
-	{
-	    test: /\.css$/i,
-	    use: [MiniCssExtractPlugin.loader, "css-loader"],
-	},
-	{
-	    test: /\.(jpe?g|png|gif|svg|mp3|svg)$/i, 
-	    type: 'asset/resource',
-	},
+    entry: './scripts/index.js',
+    output: {
+	path: path.resolve(__dirname, './dist'),
+	filename: './mpw.js',
+	library: 'MPW',
+	libraryTarget: 'var',
+    },
+    mode: "development",
+    module: {
+	rules: [
+	    {
+		test: /\.css$/i,
+		use: [MiniCssExtractPlugin.loader, "css-loader"],
+	    },
+	    {
+		test: /\.(jpe?g|png|gif|svg|mp3|svg)$/i, 
+		type: 'asset/resource',
+	    },
+	],
+    },
+    devtool: "source-map",
+    plugins: [
+	// Removes the dist/ folder before building
+	new CleanWebpackPlugin(),
+	new HtmlWebpackPlugin({
+	    template: './index.template.html',
+	    filename: 'index.html',
+	    favicon: "./assets/favicon.ico",
+	    meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
+	}),
+	// Polyfill for non web libraries
+	new NodePolyfillPlugin(),
+	// Prevents non styled flashing on load
+	new MiniCssExtractPlugin(),
+	// Make jquery available globally
+	new webpack.ProvidePlugin({
+	    $: "jquery",
+	    jQuery: "jquery",
+	    'window.jQuery': 'jquery',
+	}),
+	// Ignore non english bip39 wordlists
+	new webpack.IgnorePlugin({
+	    resourceRegExp: /^\.\/wordlists\/(?!english)/,
+	    contextRegExp: /bip39\/src$/
+	}),
     ],
-  },
-  devtool: "source-map",
-  plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-	  template: './index.template.html',
-	  filename: 'index.html',
-	  inject: "head",
-	  scriptLoading: "blocking",
-	  favicon: "./assets/favicon.ico",
-	  meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
-      }),
-      new NodePolyfillPlugin(),
-      new MiniCssExtractPlugin(),
-      new webpack.ProvidePlugin({
-	  $: "jquery",
-	  jQuery: "jquery",
-	  'window.jQuery': 'jquery',
-	  tether: 'tether',
-	  Tether: 'tether',
-	  'window.Tether': 'tether',
-      }),
-      new webpack.IgnorePlugin({
-	  resourceRegExp: /^\.\/wordlists\/(?!english)/,
-	  contextRegExp: /bip39\/src$/
-      }),
-  ],
 };
