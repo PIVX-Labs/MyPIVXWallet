1dd8ed9c9a3901cbe139389b611dc167c9cff43e
JSKitty <jskitty@protonmail.com>
2023-01-27 21:19:37 +0000

    Remove unnecessary UI

--- scripts/native.js
+++ scripts/native.js
@@ -10,42 +10,10 @@ export function registerWorker() {
         window.addEventListener('beforeinstallprompt', (event) => {
             // Prevent the mini-infobar from appearing on mobile.
             event.preventDefault();
-
-            // Stash the event so it can be triggered later.
-            window.deferredInstallPrompt = event;
-
-            // Remove the 'hidden' class from the install button container.
-            doms.domInstall.style.display = '';
-        });
-
-        // Listen for installer clicks
-        doms.domInstall.addEventListener('click', async () => {
-            const promptEvent = window.deferredInstallPrompt;
-            if (!promptEvent) {
-                // The deferred install prompt isn't available.
-                return;
-            }
-
-            // Show the install prompt.
-            promptEvent.prompt();
-
-            // Log the result
-            const result = await promptEvent.userChoice;
-            console.log(result);
-
-            // Reset the deferred prompt variable, since
-            // prompt() can only be called once.
-            window.deferredInstallPrompt = null;
-
-            // Hide the install button.
-            doms.domInstall.style.display = 'none';
         });
 
         // Listen for successful installs
         window.addEventListener('appinstalled', (_event) => {
-            // Clear the deferredPrompt so it can be garbage collected.
-            window.deferredInstallPrompt = null;
-
             // Notify!
             return createAlert('success', 'App Installed!', 2500);
         });
