09a943f36987d439ae9ea91f7b339b8177bb280b
JSKitty <jskitty@protonmail.com>
2023-07-24 21:02:35 +0100

    Prettier + Promos Correction

--- locale/pt-pt/translation.js
+++ locale/pt-pt/translation.js
@@ -28,31 +28,37 @@ export const pt_pt_translation = {
     dashboardTitle: 'Painel', //Dashboard
     dCardOneTitle: 'Criar a', //Create a
     dCardOneSubTitle: 'Nova Carteira', //New Wallet
-    dCardOneDesc: 'Crie uma carteira nova PIVX, oferecendo os métodos de backup e segurança mais seguros.', //Create a new PIVX wallet, offering the most secure backup & security methods.
+    dCardOneDesc:
+        'Crie uma carteira nova PIVX, oferecendo os métodos de backup e segurança mais seguros.', //Create a new PIVX wallet, offering the most secure backup & security methods.
     dCardOneButton: 'Crie uma Carteira Nova', //Create A New Wallet
 
     dCardTwoTitle: 'Criar Nova', //Create a new
     dCardTwoSubTitle: 'Carteira Vanity', //Vanity Wallet
-    dCardTwoDesc: 'Criar uma carteira com um prefixo personalizado, isso pode levar muito tempo!', //Create a wallet with a custom prefix, this can take a long time!
+    dCardTwoDesc:
+        'Criar uma carteira com um prefixo personalizado, isso pode levar muito tempo!', //Create a wallet with a custom prefix, this can take a long time!
     dCardTwoButton: 'Criar uma Carteira Vanity', //Create A Vanity Wallet
 
     dCardThreeTitle: 'Acesse a sua', //Access your
     dCardThreeSubTitle: 'Carteira de hardware', //Hardware Wallet
-    dCardThreeDesc: 'Use a sua carteira Ledger Hardware com a interface familiar do MPW.', //Use your Ledger Hardware wallet with MPW's familiar interface.
+    dCardThreeDesc:
+        'Use a sua carteira Ledger Hardware com a interface familiar do MPW.', //Use your Ledger Hardware wallet with MPW's familiar interface.
     dCardThreeButton: 'Acessar a minha Ledger', //Access my Ledger
 
     dCardFourTitle: 'Vá para a', //Go to
     dCardFourSubTitle: 'Minha Carteira', //My Wallet
-    dCardFourDesc: 'Importar uma carteira PIVX usando uma chave privada, xpriv ou Frase Inicial.', //Import a PIVX wallet using a Private Key, xpriv, or Seed Phrase.
+    dCardFourDesc:
+        'Importar uma carteira PIVX usando uma chave privada, xpriv ou Frase Inicial.', //Import a PIVX wallet using a Private Key, xpriv, or Seed Phrase.
     dCardFourButtonI: 'Importar Carteira', //Import Wallet
     dCardFourButtonA: 'Acessar a Minha Carteira', //Access My Wallet
 
     // Seed Phrase Creation
     thisIsYourSeed: 'Esta é a sua frase inicial:', //This is your seed phrase:
     writeDownSeed: 'Escreva em um lugar. Você só vai ver isso <b>uma vez!</b>', //Write it down somewhere. You'll only see this <b>once!</b>
-    doNotShareWarning: 'Qualquer pessoa com uma cópia dela pode acessar <b> a todos</b> os seus fundos', //Anyone with a copy of it can access <b>all</b> of your funds.
+    doNotShareWarning:
+        'Qualquer pessoa com uma cópia dela pode acessar <b> a todos</b> os seus fundos', //Anyone with a copy of it can access <b>all</b> of your funds.
     doNotShare: 'NÃO a compartilhe com ninguém.', //Do NOT share it with anybody.
-    digitalStoreNotAdvised: '<b>NÃO</b> é aconselhável armazená-lo digitalmente.', //It is <b>NOT</b> advised to store this digitally.
+    digitalStoreNotAdvised:
+        '<b>NÃO</b> é aconselhável armazená-lo digitalmente.', //It is <b>NOT</b> advised to store this digitally.
     optionalPassphrase: 'Frase Senha Opcional', //Optional Passphrase
     writtenDown: 'Eu escrevi a minha frase-inicial', //I have written down my seed phrase
 
@@ -80,14 +86,16 @@ export const pt_pt_translation = {
     privateKey: 'Chave privada', //Private Key
     viewPrivateKey: 'Mostrar a chave privada?', //View Private Key?
     privateWarning1: 'Certifique-se de que ninguém está a ver o seu ecra.', //Make sure no one can see your screen.
-    privateWarning2: 'Qualquer pessoa com esta chave pode roubar os seus fundos', //Anyone with this key can steal your funds.
+    privateWarning2:
+        'Qualquer pessoa com esta chave pode roubar os seus fundos', //Anyone with this key can steal your funds.
     viewKey: 'Ver a chave', //View key
 
     // PIVX Promos
     // NOTE: for this below line, continue the sentence "PIVX Promos [...]", since 'PIVX Promos' is a brand, it is not translated.
-    pivxPromos: '[...] um sistema descentralizado de códigos de presente no valor de PIV', // [...] is a decentralised system for gift codes worth PIV
+    pivxPromos:
+        'um sistema descentralizado de códigos de presente no valor de PIV', // [...] is a decentralised system for gift codes worth PIV
     // NOTE: on this line, if possible, leave 'PIVX Promos' untranslated
-    redeemInput: 'Digite o seu código \'PIVX Promos\'', //Enter your 'PIVX Promos' code
+    redeemInput: "Digite o seu código 'PIVX Promos'", //Enter your 'PIVX Promos' code
     createName: 'Nome da promoção (opcional)', //Promo Name (Optional)
     createAmount: 'Valor promocional', //Promo Amount
 
@@ -99,10 +107,12 @@ export const pt_pt_translation = {
 
     // Masternode
     mnControlYour: 'Controle o seu', //Control your
-    mnSubtext: 'A partir deste guia, você pode criar e acessar um ou mais masternodes', //From this tab you can create and access one or more masternodes
+    mnSubtext:
+        'A partir deste guia, você pode criar e acessar um ou mais masternodes', //From this tab you can create and access one or more masternodes
 
     // Governance
-    govSubtext: 'Nesta aba você pode conferir as propostas e, se tiver um masternode, fazer parte do <b>DAO</b> e votar!', //From this tab you can check the proposals and, if you have a masternode, be a part of the <b>DAO</b> and vote!
+    govSubtext:
+        'Nesta aba você pode conferir as propostas e, se tiver um masternode, fazer parte do <b>DAO</b> e votar!', //From this tab you can check the proposals and, if you have a masternode, be a part of the <b>DAO</b> and vote!
     govMonthlyBudget: 'Orçamento mensal', //Monthly Budget
     govAllocBudget: 'Orçamento Alocado', //Allocated Budget
     govNextPayout: 'Próximo Pagamento da  Tesoraria', //Next Treasury Payout
@@ -112,7 +122,8 @@ export const pt_pt_translation = {
     govTableVotes: 'VOTOS', //VOTES
     govTableVote: 'VOTE', //VOTE
     contestedProposalsTitle: 'Propostas Contestadas', //Contested Proposals
-    contestedProposalsDesc: 'Essas são as propostas que receberam uma quantidade esmagadora de votos negativos, tornando-as provavelmente spam ou uma proposta altamente contestável.', //These are proposals that received an overwhelming amount of downvotes, making it likely spam or a highly contestable proposal.
+    contestedProposalsDesc:
+        'Essas são as propostas que receberam uma quantidade esmagadora de votos negativos, tornando-as provavelmente spam ou uma proposta altamente contestável.', //These are proposals that received an overwhelming amount of downvotes, making it likely spam or a highly contestable proposal.
 
     // Settings
     settingsCurrency: 'Escolha uma moeda de exibição:', //Choose a display currency:
@@ -127,8 +138,10 @@ export const pt_pt_translation = {
     // Transparency Report
     transparencyReport: '"Relatório de Transparência"', //"Transparency Report",
     hit: '"Um ping a indicar o carregamento de um aplicativo, nenhum dado exclusivo é enviado."', //"A ping indicating an app load, no unique data is sent.",
-    time_to_sync: '"O tempo em segundos que o MPW levou para sincronizar pela última vez."', //"The time in seconds it took for MPW to last synchronise.",
-    transaction: '"Um ping indicando um Tx, nenhum dado exclusivo é enviado, mas pode ser inferido a partir do tempo na rede."', //"A ping indicating a Tx, no unique data is sent, but may be inferred from on-chain time.",
+    time_to_sync:
+        '"O tempo em segundos que o MPW levou para sincronizar pela última vez."', //"The time in seconds it took for MPW to last synchronise.",
+    transaction:
+        '"Um ping indicando um Tx, nenhum dado exclusivo é enviado, mas pode ser inferido a partir do tempo na rede."', //"A ping indicating a Tx, no unique data is sent, but may be inferred from on-chain time.",
 
     // Alerts
     ALERTS: '<-- DO NOT EDIT! All below entries are for Alert Popups',
