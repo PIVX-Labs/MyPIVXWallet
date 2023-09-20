76a82667e0dd157e8ca4d6160012e5aedcf854f9
Duddino <duddino@duddino.com>
2023-09-20 20:07:59 +0200

    Prettier

--- scripts/settings.js
+++ scripts/settings.js
@@ -275,7 +275,7 @@ export async function setExplorer(explorer, fSilent = false) {
     // Enable networking + notify if allowed
     const network = new ExplorerNetwork(cExplorer.url, wallet.getMasterKey());
     setNetwork(network);
-    
+
     activityDashboard.reset();
     stakingDashboard.reset();
 
