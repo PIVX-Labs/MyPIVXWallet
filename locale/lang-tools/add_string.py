8e9fbf5450306a2f7ea3a33dfdde03c521b3e471
Duddino <duddino@duddino.com>
2023-10-24 15:48:51 +0200

    Rename parent_path

--- locale/lang-tools/add_string.py
+++ locale/lang-tools/add_string.py
@@ -43,10 +43,10 @@ def copy_template(template_path, locale_path, comment, merge_files):
             # TODO: add merging support to more than 2 languages
             if len(paths) == 2:
                 split_path = paths[0].split('/')
-                new_path = '/'.join(split_path[:2] + [split_path[-2].split('-')[0]] + [split_path[-1]])
-                merge(paths[0], paths[1], new_path)
+                parent_path = '/'.join(split_path[:2] + [split_path[-2].split('-')[0]] + [split_path[-1]])
+                merge(paths[0], paths[1], parent_path)
                 if comment:
-                    comment_file(new_path, template_path)
+                    comment_file(parent_path, template_path)
                     comment_file(paths[0], template_path)
                     comment_file(paths[1], template_path)
             
