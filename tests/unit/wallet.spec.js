1c3931546c8ad1531160c4b38c22ffab89dd733f
Duddino <duddino@duddino.com>
2024-01-18 13:32:56 +0100

    Add wallet shield tests

--- tests/unit/wallet.spec.js
+++ tests/unit/wallet.spec.js
@@ -9,6 +9,7 @@ import {
     Transaction,
 } from '../../scripts/transaction.js';
 import { mempool } from '../../scripts/global';
+import { hexToBytes } from '../../scripts/utils';
 
 vi.mock('../../scripts/global.js', (g) => {
     const Mempool = vi.fn();
@@ -217,6 +218,16 @@ describe('Wallet transaction tests', () => {
 });
 
 describe('Wallet signature tests', () => {
+    let PIVXShield;
+    beforeEach(() => {
+        PIVXShield = vi.fn();
+        PIVXShield.prototype.createTransaction = vi.fn(() => {
+            return {
+                hex: '00',
+            };
+        });
+    });
+
     it('throws when is view only', async () => {
         const wallet = new Wallet(0, false);
         const mk = getLegacyMainnet();
@@ -257,5 +268,125 @@ describe('Wallet signature tests', () => {
         expect(signedTx.txid).toBe(
             '9cf01cffc85d53b80a9c7ca106fc7326efa0f4f1db3eaf5be0ac45eb6105b8ab'
         );
+    });
+
+    it('signs a s->s transaction correctly', async () => {
+        const wallet = new Wallet(0, false);
+        wallet.setMasterKey(getLegacyMainnet());
+        wallet.setShield(new PIVXShield());
+        const tx = new Transaction({
+            version: 3,
+            blockHeight: -1,
+            vin: [],
+            vout: [],
+            shieldData: [
+                {
+                    value: 100000,
+                    address: 'ptest1234567',
+                },
+            ],
+        });
+        const txRef = await wallet.sign(tx);
+        expect(txRef).toBe(tx);
+        expect(PIVXShield.prototype.createTransaction).toHaveBeenCalledWith({
+            address: 'ptest1234567',
+            amount: 100000,
+            blockHeight: 1504903,
+            transparentChangeAddress: 'DTSTGkncpC86sbEUZ2rCBLEe2aXSeZPLnC',
+            useShieldInputs: true, // Because vin is empty
+            utxos: [
+                {
+                    amount: 10000000,
+                    private_key: getLegacyMainnet().getPrivateKeyBytes(),
+                    script: hexToBytes(
+                        '76a914f49b25384b79685227be5418f779b98a6be4c73888ac'
+                    ),
+                    txid: 'f8f968d80ac382a7b64591cc166489f66b7c4422f95fbd89f946a5041d285d7c',
+                    vout: 1,
+                },
+            ],
+        });
+    });
+    it('signs a s->t tx correctly', async () => {
+        const wallet = new Wallet(0, false);
+        wallet.setMasterKey(getLegacyMainnet());
+        wallet.setShield(new PIVXShield());
+        const tx = new Transaction({
+            version: 3,
+            blockHeight: -1,
+            vin: [],
+            vout: [
+                new CTxOut({
+                    script: '76a914f49b25384b79685227be5418f779b98a6be4c73888ac',
+                    value: 4992400,
+                }),
+            ],
+            shieldData: [],
+        });
+        const txRef = await wallet.sign(tx);
+        expect(txRef).toBe(tx);
+        expect(PIVXShield.prototype.createTransaction).toHaveBeenCalledWith({
+            address: 'DTSTGkncpC86sbEUZ2rCBLEe2aXSeZPLnC',
+            amount: 4992400,
+            blockHeight: 1504903,
+            transparentChangeAddress: 'DTSTGkncpC86sbEUZ2rCBLEe2aXSeZPLnC',
+            useShieldInputs: true, // Because vin is empty
+            utxos: [
+                {
+                    amount: 10000000,
+                    private_key: getLegacyMainnet().getPrivateKeyBytes(),
+                    script: hexToBytes(
+                        '76a914f49b25384b79685227be5418f779b98a6be4c73888ac'
+                    ),
+                    txid: 'f8f968d80ac382a7b64591cc166489f66b7c4422f95fbd89f946a5041d285d7c',
+                    vout: 1,
+                },
+            ],
+        });
+    });
+    it('signs a t->s tx correctly', async () => {
+        const wallet = new Wallet(0, false);
+        wallet.setMasterKey(getLegacyMainnet());
+        wallet.setShield(new PIVXShield());
+        const tx = new Transaction({
+            version: 3,
+            blockHeight: -1,
+            vin: [
+                new CTxIn({
+                    outpoint: new COutpoint({
+                        txid: 'f8f968d80ac382a7b64591cc166489f66b7c4422f95fbd89f946a5041d285d7c',
+                        n: 1,
+                    }),
+                    script: '76a914f49b25384b79685227be5418f779b98a6be4c73888ac',
+                }),
+            ],
+            vout: [],
+            shieldData: [
+                {
+                    value: 100000,
+                    address: 'ptest1234567',
+                },
+            ],
+        });
+        const txRef = await wallet.sign(tx);
+        expect(txRef).toBe(tx);
+        expect(PIVXShield.prototype.createTransaction).toHaveBeenCalledWith({
+            address: 'ptest1234567',
+            amount: 100000,
+            blockHeight: 1504903,
+            transparentChangeAddress: 'DTSTGkncpC86sbEUZ2rCBLEe2aXSeZPLnC',
+            useShieldInputs: false,
+            utxos: [
+                {
+                    amount: 10000000,
+                    private_key: getLegacyMainnet().getPrivateKeyBytes(),
+                    script: hexToBytes(
+                        '76a914f49b25384b79685227be5418f779b98a6be4c73888ac'
+                    ),
+                    txid: 'f8f968d80ac382a7b64591cc166489f66b7c4422f95fbd89f946a5041d285d7c',
+                    vout: 1,
+                },
+            ],
+        });
     });
 });
