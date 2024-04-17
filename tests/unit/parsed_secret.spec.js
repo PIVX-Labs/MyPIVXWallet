a8cd003712e5df10515e8afdcc71e5a5d793f489
Duddino <duddino@duddino.com>
2024-04-17 18:37:46 +0200

    Add space tests to seed phrases

--- tests/unit/parsed_secret.spec.js
+++ tests/unit/parsed_secret.spec.js
@@ -18,26 +18,31 @@ describe('Parsed secret tests', () => {
     });
     it.each(secretTestCases)(
         'parses secret $secret',
-        async ({ secret, password, expected }) => {
-            const parsedSecret = await ParsedSecret.parse(secret, password);
-            if (expected.xpriv)
-                expect(parsedSecret.masterKey?.keyToBackup ?? null).toBe(
-                    expected.xpriv
-                );
-            if (expected.xpub)
-                expect(parsedSecret.masterKey?.getKeyToExport(0) ?? null).toBe(
-                    expected.xpub
-                );
-            const { seed, extendedSpendingKey: extsk } =
-                pivxShield.PIVXShield.create.mock.calls.at(-1)?.at(0) ?? {};
-
-            if (seed) expect(bytesToHex(seed)).toBe(expected.seed);
-            else {
-                expect(expected.seed).toBeUndefined();
+        async ({ secret: secrets, password, expected }) => {
+            if (!Array.isArray(secrets)) {
+                secrets = [secrets];
             }
-            if (extsk) expect(extsk).toBe(expected.extsk);
-            else {
-                expect(expected.extsk).toBeUndefined();
+            for (const secret of secrets) {
+                const parsedSecret = await ParsedSecret.parse(secret, password);
+                if (expected.xpriv)
+                    expect(parsedSecret.masterKey?.keyToBackup ?? null).toBe(
+                        expected.xpriv
+                    );
+                if (expected.xpub)
+                    expect(
+                        parsedSecret.masterKey?.getKeyToExport(0) ?? null
+                    ).toBe(expected.xpub);
+                const { seed, extendedSpendingKey: extsk } =
+                    pivxShield.PIVXShield.create.mock.calls.at(-1)?.at(0) ?? {};
+
+                if (seed) expect(bytesToHex(seed)).toBe(expected.seed);
+                else {
+                    expect(expected.seed).toBeUndefined();
+                }
+                if (extsk) expect(extsk).toBe(expected.extsk);
+                else {
+                    expect(expected.extsk).toBeUndefined();
+                }
             }
         }
     );
