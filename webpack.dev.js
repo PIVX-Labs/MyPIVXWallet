eb6dd3f90f17415067922359d51af20102aaa7c4
JSKitty <jskitty@protonmail.com>
2024-04-22 14:26:07 +0100

    Ignore Node Modules and Coverage directories
    Co-authored-by: Duddino <duddino@duddino.com>

--- webpack.dev.js
+++ webpack.dev.js
@@ -26,6 +26,9 @@ export default merge(common, {
                 ignored: [
                     // ignore changes in '.git' subdirectory (prevent constant hot-reloading in auto-fetch configurations)
                     '**/.git',
+                    /node_modules/,
+		    /coverage/,
+
                 ],
             },
         },
