69cc967003663c113e58be9053a4ec36d07fcd42
Duddino <duddino@duddino.com>
2024-04-29 16:27:02 +0200

    Polyfill nodejs crypto with noble

---
+++ scripts/polyfills/crypto.js
@@ -0,0 +1,39 @@
+// Lightweight polyfill for the NodeJS crypto module
+import { sha256 } from '@noble/hashes/sha256';
+import { sha512 } from '@noble/hashes/sha512';
+import { hmac } from '@noble/hashes/hmac';
+import { randomBytes } from '@noble/hashes/utils';
+import { Buffer } from 'buffer';
+
+export const createHash = (hash, options) => {
+    if (!!options) throw new Error('Unfilled polyfill');
+    let fun;
+    switch (hash) {
+        case 'sha256':
+            fun = sha256;
+            break;
+        case 'sha1':
+            fun = sha1;
+            break;
+        default:
+            throw new Error('Unfilleld polyfill');
+    }
+    return {
+        update: (buff) => {
+            return {
+                digest: () => {
+                    return Buffer.from(fun(buff));
+                },
+            };
+        },
+    };
+};
+
+export const createHmac = (hash, key) => {
+    if (hash !== 'sha512') throw new Error('unfilled polyfill');
+    return hmac.create(sha512, key);
+};
+
+export const randomBytes = (length) => {
+    return new Buffer.from(randomBytes(length));
+};
