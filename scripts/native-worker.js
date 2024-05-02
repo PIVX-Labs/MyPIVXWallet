5a70fcbff40f4a63f5eaa183bef0a09c713cec17
Duddino <duddino@duddino.com>
2024-05-02 16:10:12 +0200

    Prefetch this too

--- scripts/native-worker.js
+++ scripts/native-worker.js
@@ -19,7 +19,7 @@ self.addEventListener('fetch', (event) => {
     // for non-GET requests.
     if (event.request.method !== 'GET') return;
 
-    const cacheRegexps = [/sapling-(spend|output)\.params/, /(multicore|util)/];
+    const cacheRegexps = [/sapling-(spend|output)\.params/, /(multicore|util|pivx-shield)/];
 
     if (!cacheRegexps.some((r) => r.test(event.request.url))) {
         return;
