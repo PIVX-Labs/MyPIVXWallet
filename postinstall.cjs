58c1a2d19446ad3912db19f5949fad2417b7fc48
Duddino <duddino@duddino.com>
2024-07-23 14:30:10 +0200

    Add other OSes support to postinstall.cjs

--- postinstall.cjs
+++ postinstall.cjs
@@ -1,26 +1,27 @@
 const { execSync } = require('child_process');
-const fs = require('fs');
 
 // Determine the operating system type
 const os = require('os');
-const osType = os.type();
+const osType = os.platform();
 
 // Define the command to execute based on the OS
 let command;
-if (osType === 'Linux') {
-  command = 'test -f chain_params.json || cp chain_params.prod.json chain_params.json';
-} else if (osType === 'Windows_NT') {
-  command = 'if not exist chain_params.json copy chain_params.prod.json chain_params.json';
+if (osType.match(/aix|darwin|freebsd|linux|openbsd|sunos|android/)) {
+    command =
+        'test -f chain_params.json || cp chain_params.prod.json chain_params.json';
+} else if (osType.match(/win32/)) {
+    command =
+        'if not exist chain_params.json copy chain_params.prod.json chain_params.json';
 } else {
-  console.error('Unsupported operating system');
-  process.exit(1);
+    console.error('Unsupported operating system');
+    process.exit(1);
 }
 
 // Execute the command
 try {
-  execSync(command, { stdio: 'inherit' });
-  console.log('Postinstall script executed successfully');
+    execSync(command, { stdio: 'inherit' });
+    console.log('Postinstall script executed successfully');
 } catch (error) {
-  console.error('Error executing postinstall script:', error);
-  process.exit(1);
+    console.error('Error executing postinstall script:', error);
+    process.exit(1);
 }
