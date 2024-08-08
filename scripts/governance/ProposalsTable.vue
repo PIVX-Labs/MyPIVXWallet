515e7a32a3d61e8dac7213d8a78f370ef0e858c9
Duddino <duddino@duddino.com>
2024-08-08 11:07:40 +0200

    Finish governance table for mobile

--- scripts/governance/ProposalsTable.vue
+++ scripts/governance/ProposalsTable.vue
@@ -3,7 +3,8 @@ import ProposalStatus from './ProposalStatus.vue';
 import ProposalName from './ProposalName.vue';
 import ProposalPayment from './ProposalPayment.vue';
 import ProposalVotes from './ProposalVotes.vue';
-import { toRefs, computed } from 'vue';
+import MobileProposalRow from './MobileProposalRow.vue';
+import { toRefs, computed, ref } from 'vue';
 import { translation } from '../i18n.js';
 import { ProposalValidator } from './status';
 const props = defineProps({
@@ -16,6 +17,15 @@ const { proposals, masternodeCount, strCurrency, price } = toRefs(props);
 const proposalValidator = computed(
     () => new ProposalValidator(masternodeCount.value)
 );
+
+/**
+ * @type{import('vue').Ref<number?>} Index of the row opened on mobile mode. Null if nothing is open
+ */
+const opened = ref(null);
+
+function openOrCloseRow(i) {
+    opened.value = opened.value === i ? null : i;
+}
 
 //TODO: fetch currency from settings
 </script>
@@ -48,45 +58,53 @@ const proposalValidator = computed(
             id="proposalsTableBody"
             style="text-align: center; vertical-align: middle"
         >
-            <tr v-for="(proposal, index) of proposals">
-                <td class="governStatusCol">
-                    <!-- REMEMBER TO UPDATE THIS!!! -->
-                    <ProposalStatus
-                        :proposal="proposal"
-                        :proposalValidator="proposalValidator"
-                        :nMasternodes="masternodeCount"
-                    />
-                </td>
-                <td style="vertical-align: middle">
-                    <ProposalName :proposal="proposal" />
-                </td>
-                <td style="vertical-align: middle">
-                    <ProposalPayment
-                        :proposal="proposal"
-                        :price="price"
-                        :strCurrency="strCurrency"
-                    />
-                </td>
-                <td style="vertical-align: middle">
-                    <ProposalVotes :proposal="proposal" />
-                </td>
-                <td style="vertical-align: middle">
-                    <div class="proposalVoteButtons for-desktop">
-                        <div
-                            class="pivx-button-outline pivx-button-outline-small govNoBtnMob"
-                            style="width: fit-content"
-                        >
-                            <span> {{ translation.no }} </span>
+            <template v-for="(proposal, i) of proposals">
+                <tr>
+                    <td class="governStatusCol" @click="openOrCloseRow(i)">
+                        <!-- REMEMBER TO UPDATE THIS!!! -->
+                        <ProposalStatus
+                            :proposal="proposal"
+                            :proposalValidator="proposalValidator"
+                            :nMasternodes="masternodeCount"
+                        />
+                    </td>
+                    <td style="vertical-align: middle">
+                        <ProposalName :proposal="proposal" />
+                    </td>
+                    <td style="vertical-align: middle" class="for-desktop">
+                        <ProposalPayment
+                            :proposal="proposal"
+                            :price="price"
+                            :strCurrency="strCurrency"
+                        />
+                    </td>
+                    <td style="vertical-align: middle" class="for-desktop">
+                        <ProposalVotes :proposal="proposal" />
+                    </td>
+                    <td style="vertical-align: middle" class="for-desktop">
+                        <div class="proposalVoteButtons">
+                            <div
+                                class="pivx-button-outline pivx-button-outline-small govNoBtnMob"
+                                style="width: fit-content"
+                            >
+                                <span> {{ translation.no }} </span>
+                            </div>
+                            <div
+                                class="pivx-button-small govYesBtnMob"
+                                style="width: fit-content"
+                            >
+                                <span> {{ translation.yes }} </span>
+                            </div>
                         </div>
-                        <div
-                            class="pivx-button-small govYesBtnMob"
-                            style="width: fit-content"
-                        >
-                            <span> {{ translation.yes }} </span>
-                        </div>
-                    </div>
-                </td>
-            </tr>
+                    </td>
+                </tr>
+                <MobileProposalRow
+                    v-if="opened == i"
+                    :proposal="proposal"
+                    :price="price"
+                    :strCurrency="strCurrency"
+                />
+            </template>
         </tbody>
     </table>
 </template>
