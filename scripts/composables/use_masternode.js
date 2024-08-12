c68853a3810c64867799ba0a6d31a4a025b7175c
Duddino <duddino@duddino.com>
2024-08-12 15:47:13 +0200

    Add use_masternode

---
+++ scripts/composables/use_masternode.js
@@ -0,0 +1,25 @@
+import { ref, watch } from 'vue';
+import { defineStore } from 'pinia';
+import Masternode from '../masternode.js';
+import { Database } from '../database.js';
+import { getEventEmitter } from '../event_bus.js';
+
+export const useMasternode = defineStore('masternode', () => {
+    /**
+     * @type{import('vue').Ref<Masternode?>}
+     */
+    const masternode = ref(null);
+    const fetchMasternodeFromDatabase = async () => {
+        const database = await Database.getInstance();
+        masternode.value = await database.getMasternode();
+    };
+    fetchMasternodeFromDatabase().then(() => {});
+    getEventEmitter().on('toggle-network', () => {
+        fetchMasternodeFromDatabase().then(() => {});
+    });
+    watch(masternode, async () => {
+        const database = await Database.getInstance();
+        await database.addMasternode(masternode.value);
+    });
+    return { masternode };
+});
