bb4450f26a651462601a2743e844975905f1a47d
Duddino <duddino@duddino.com>
2024-08-27 15:40:53 +0200

    Add ledger persistance

--- scripts/composables/use_wallet.js
+++ scripts/composables/use_wallet.js
@@ -32,12 +32,13 @@ export const useWallet = defineStore('wallet', () => {
     const isViewOnly = ref(wallet.isViewOnly());
     const isSynced = ref(wallet.isSynced);
     const getKeyToBackup = async () => await wallet.getKeyToBackup();
+    const getKeyToExport = () => wallet.getKeyToExport();
     const isEncrypted = ref(true);
     const loadFromDisk = () => wallet.loadFromDisk();
     const hasShield = ref(wallet.hasShield());
 
     const setMasterKey = async ({ mk, extsk }) => {
-        wallet.setMasterKey({ mk, extsk });
+        await wallet.setMasterKey({ mk, extsk });
         isImported.value = wallet.isLoaded();
         isHardwareWallet.value = wallet.isHardwareWallet();
         isHD.value = wallet.isHD();
@@ -124,6 +125,7 @@ export const useWallet = defineStore('wallet', () => {
         isEncrypted,
         isSynced,
         getKeyToBackup,
+        getKeyToExport,
         setMasterKey,
         setExtsk,
         setShield,
