3bb01e02b827e052b24f17d7afaee2745e7b9de7
Duddino Gatto <duddinogatto@pm.me>
2024-09-09 19:38:33 +0200

    Add { instead

--- scripts/lock.js
+++ scripts/lock.js
@@ -2,13 +2,10 @@
  * Implement a lockable object
  * @template T
  * @param {T} f - the function on which we perform the lock
- * @returns T & { isLocked: () => bool }
+ * @returns {T & { isLocked: () => bool }}
  */
 export const lockableFunction = (f) => {
     let lock = false;
-    /**
-     * @type{T & {isLocked: () => bool}}
-     */
     const g = async (...args) => {
         try {
             if (!lock) {
