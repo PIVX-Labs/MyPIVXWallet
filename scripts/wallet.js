cc1a38f16312e2f2b0792e7274316aeef1582d57
Duddino <duddino@duddino.com>
2024-10-27 16:18:06 +0100

    Fix activity time

--- scripts/wallet.js
+++ scripts/wallet.js
@@ -871,7 +871,8 @@ export class Wallet {
                         for (const tx of block.txs) {
                             const parsed = Transaction.fromHex(tx.hex);
                             parsed.blockHeight = blockHeight;
-                            parsed.blockTime = tx.blocktime;
+                            // Median time is in
+                            parsed.blockTime = block.mediantime;
                             // Avoid wasting memory on txs that do not regard our wallet
                             if (this.ownTransaction(parsed)) {
                                 await this.addTransaction(parsed);
