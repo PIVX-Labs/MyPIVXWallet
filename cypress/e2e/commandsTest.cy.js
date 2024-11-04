24ea044ccc94a504741effd1b462395ca0521642
Duddino <duddino@duddino.com>
2024-11-04 15:34:35 +0100

    Add e2e testing to CI

--- cypress/e2e/commandsTest.cy.js
+++
@@ -1,9 +0,0 @@
-describe('template spec', () => {
-    it('passes', () => {
-        cy.clearDb();
-        cy.visit('/');
-        cy.createWallet('123456');
-        cy.goToTab('stake');
-        cy.deleteWallet();
-    });
-});
