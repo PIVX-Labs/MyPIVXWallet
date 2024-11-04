0def3c050776fcfaf2eb168e4b9360c4715a98ff
Duddino <duddino@duddino.com>
2024-11-04 14:48:23 +0100

    Add wallet balance and activity test

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'cypress';
-
+import { existsSync, readFileSync } from 'fs';
+import cypressPlayback from '@oreillymedia/cypress-playback/addTasks.js';
+import { addMatchImageSnapshotPlugin } from '@simonsmith/cypress-image-snapshot/plugin.js';
 export default defineConfig({
     e2e: {
         baseUrl: 'http://127.0.0.1:5500',
+        setupNodeEvents(on, config) {
+            cypressPlayback(on, config);
+            addMatchImageSnapshotPlugin(on);
+        },
     },
 });
