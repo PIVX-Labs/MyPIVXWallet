302ee1b21e210a9608adbeb7b5ab2ebdec760474
Alessandro Rezzi <alessandrorezzi2000@gmail.com>
2024-11-08 08:37:40 +0100

    feat: Choose nodes and explorer randomly

--- scripts/settings.js
+++ scripts/settings.js
@@ -20,6 +20,7 @@ import { Database } from './database.js';
 import { getEventEmitter } from './event_bus.js';
 import countries from 'country-locale-map/countries.json';
 import { getNetwork } from './network/network_manager.js';
+import { getRandomElement } from './utils.js';
 
 // --- Default Settings
 /** A mode that emits verbose console info for internal MPW operations */
@@ -433,7 +434,7 @@ export async function toggleAutoSwitch() {
 }
 
 async function fillExplorerSelect() {
-    const firstExplorer = cChainParams.current.Explorers[0];
+    const firstExplorer = getRandomElement(cChainParams.current.Explorers);
 
     while (doms.domExplorerSelect.options.length > 0) {
         doms.domExplorerSelect.remove(0);
@@ -452,7 +453,7 @@ async function fillExplorerSelect() {
     const database = await Database.getInstance();
     const { explorer: strSettingExplorer } = await database.getSettings();
 
-    // For any that exist: load them, or use the defaults
+    // For any that exist: load them, or use the default
     await setExplorer(
         cChainParams.current.Explorers.find(
             (a) => a.url === strSettingExplorer
@@ -462,7 +463,7 @@ async function fillExplorerSelect() {
 }
 
 async function fillNodeSelect() {
-    const firstNode = cChainParams.current.Nodes[0];
+    const firstNode = getRandomElement(cChainParams.current.Nodes);
 
     while (doms.domNodeSelect.options.length > 0) {
         doms.domNodeSelect.remove(0);
