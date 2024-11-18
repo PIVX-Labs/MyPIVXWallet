acb7102a031fdfe905ab4f6c42326494d6907c81
JSKitty <mail@jskitty.cat>
2024-11-18 10:33:32 +0000

    Prettier

--- scripts/alerts/Alert.vue
+++ scripts/alerts/Alert.vue
@@ -30,15 +30,24 @@ const icon = computed(() => {
         :style="{ opacity: 1 }"
         data-testid="alert"
     >
-        <div class="notifyBadgeCount" v-if="notificationCount > 1" v-html="notificationCount"></div>
-        <div style="display: inline-flex; align-items: stretch;">
+        <div
+            class="notifyBadgeCount"
+            v-if="notificationCount > 1"
+            v-html="notificationCount"
+        ></div>
+        <div style="display: inline-flex; align-items: stretch">
             <div class="notifyIcon" :class="{ ['notify-' + level]: true }">
                 <i class="fas fa-xl" :class="{ [icon]: true }"> </i>
             </div>
             <div class="notifyText" v-html="message"></div>
         </div>
-        <div style="display:flex; flex-direction: column;">
-            <button class="btn btn-notification-close" @click="$emit('hideAlert')">CLOSE</button>
+        <div style="display: flex; flex-direction: column">
+            <button
+                class="btn btn-notification-close"
+                @click="$emit('hideAlert')"
+            >
+                CLOSE
+            </button>
         </div>
     </div>
 </template>
