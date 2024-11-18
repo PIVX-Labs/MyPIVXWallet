37c3c1a7a789140b5ba96cbd4bebb59f1776fdff
BreadJS <83626012+BreadJS@users.noreply.github.com>
2024-11-18 09:27:09 +0100

    [VUE] New alert layout

--- scripts/alerts/Alert.vue
+++ scripts/alerts/Alert.vue
@@ -25,14 +25,18 @@ const icon = computed(() => {
 <template>
     <div
         class="notifyWrapper"
-        @click="$emit('click')"
         :class="{ [level]: true }"
         :style="{ opacity: 1 }"
         data-testid="alert"
     >
-        <div class="notifyIcon" :class="{ ['notify-' + level]: true }">
-            <i class="fas fa-xl" :class="{ [icon]: true }"> </i>
+        <div style="display: inline-flex; align-items: stretch;">
+            <div class="notifyIcon" :class="{ ['notify-' + level]: true }">
+                <i class="fas fa-xl" :class="{ [icon]: true }"> </i>
+            </div>
+            <div class="notifyText" v-html="message"></div>
         </div>
-        <div class="notifyText" v-html="message"></div>
+        <div style="display:flex; flex-direction: column;">
+            <button class="btn btn-notification-close" @click="$emit('hideAlert')">CLOSE</button>
+        </div>
     </div>
 </template>
