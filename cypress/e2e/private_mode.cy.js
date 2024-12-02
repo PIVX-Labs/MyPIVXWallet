d091ac872db643b22b2205eb384a1722a18fa2a8
Duddino <duddino@duddino.com>
2024-12-02 11:29:57 +0100

    Finally works gg

--- cypress/e2e/private_mode.cy.js
+++ cypress/e2e/private_mode.cy.js
@@ -1,9 +1,13 @@
 describe('public/private mode tests', () => {
     const before = () => {
-        cy.clearDb();
         cy.playback('GET', /(xpub|address|getshielddata|duddino|block)/, {
             matching: { ignores: ['hostname', 'port'] },
         }).as('sync');
+        cy.intercept('GET', 'https://rpc.duddino.com/mainnet/getblockcount', {
+            statusCode: 200,
+            body: 4669846,
+        });
+        cy.clearDb();
         cy.visit('/');
         cy.waitForLoading().should('be.visible');
 
@@ -17,21 +21,18 @@ describe('public/private mode tests', () => {
     };
 
     it('switches back to public mode when not available', () => {
+        // It should remember private mode
         before();
+        cy.visit('/');
+        cy.waitForSync();
+
+        cy.get('[data-testid="shieldModePrefix"]').should('exist');
         // We should be in private mode here
         cy.get('[data-testid="shieldModePrefix"]').should('exist');
         cy.deleteWallet();
         // When importing a non shield capable wallet, we should be in public mode
         cy.importWallet('DLabsktzGMnsK5K9uRTMCF6NoYNY6ET4Bb');
         cy.waitForSync();
-        cy.wait('@sync');
         cy.get('[data-testid="shieldModePrefix"]').should('not.exist');
-    });
-
-    it('remembers private mode', () => {
-        before();
-        cy.visit('/');
-        cy.waitForSync();
-        cy.get('[data-testid="shieldModePrefix"]').should('exist');
     });
 });
