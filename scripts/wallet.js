c778f5e67a620ad3d171bd1500a550ce43271160
Duddino <duddino@duddino.com>
2025-04-30 15:50:16 +0200

    Remove debugger

--- scripts/wallet.js
+++ scripts/wallet.js
@@ -823,7 +823,6 @@ export class Wallet {
         return this.#historicalTxs.get();
     }
     sync = lockableFunction(async () => {
-        debugger;
         if (this.#isSynced) {
             throw new Error('Attempting to sync when already synced');
         }
