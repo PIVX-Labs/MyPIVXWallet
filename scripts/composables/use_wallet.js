17418d151856db3212fc41af5cea9659ce15c223
Duddino <duddino@duddino.com>
2025-10-14 15:22:29 +0200

    Improve ledger address confirmation screen

--- scripts/composables/use_wallet.js
+++ scripts/composables/use_wallet.js
@@ -85,6 +85,7 @@ function addWallet(wallet) {
                 const [changeAddress] = await guiGetNewAddress({
                     verify: true,
                     nReceiving: 0,
+                    isForChange: true,
                 });
                 tx = wallet.createTransaction(address, value, {
                     ...opts,
