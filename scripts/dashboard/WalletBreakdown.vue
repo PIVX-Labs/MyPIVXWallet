cf4d3d95e9f7fa1e12007dff0f76a3e064b9c0f5
Duddino <duddino@duddino.com>
2025-10-28 20:18:50 +0100

    Improve colors etc

--- scripts/dashboard/WalletBreakdown.vue
+++ scripts/dashboard/WalletBreakdown.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Color from 'color';
 import {
     ArcElement,
     Chart,
@@ -13,17 +14,31 @@ import { useTemplateRef, ref } from 'vue';
 import { useWallets } from '../composables/use_wallet.js';
 import { Database } from '../database.js';
 import { cChainParams, COIN } from '../chain_params';
-import { onMounted } from 'vue';
+import { onMounted, watch } from 'vue';
 import { beautifyNumber } from '../misc';
 import { translation } from '../i18n';
+import { shallowRef } from 'vue';
+import { onUnmounted } from 'vue';
 
 const canvas = useTemplateRef('canvas');
 const arrBreakdown = ref([]);
-const chartWalletBreakdown = ref();
+const chartWalletBreakdown = shallowRef();
 
-const { activeWallet } = useWallets();
+const { vaults } = useWallets();
 const emit = defineEmits(['close']);
 const isGenerated = ref(false);
+
+const baseColours = [
+    '#2196F3',
+    '#3A0CA3',
+    '#D62828',
+    '#6D6875',
+    '#E63946',
+    '#F4A261',
+    '#E9C46A',
+    '#A7C957',
+    '#2A9D8F',
+];
 
 /**
  * An element generated from the wallet for the purpose of charting or tables
@@ -38,79 +53,89 @@ const isGenerated = ref(false);
  * @returns {Promise<Array<WalletDatasetPoint>>} - The charting data
  */
 async function getWalletDataset() {
-    const arrBreakdown = [];
+    const res = [];
+    let walletNumber = 0;
+    for (const vault of vaults) {
+        for (const [i, activeWallet] of vault.wallets.entries()) {
+            const baseColour = Color(
+                baseColours[walletNumber++ % baseColours.length]
+            );
+            // Public (Available)
+            const spendable_bal = activeWallet.balance;
+            if (spendable_bal > 0) {
+                res.push({
+                    type: `${translation.chartPublicAvailable}-${vault.label}-${i}`,
+                    balance: spendable_bal / COIN,
+                    colour: baseColour.toString(),
+                });
+            }
 
-    // Public (Available)
-    const spendable_bal = activeWallet.balance;
-    if (spendable_bal > 0) {
-        arrBreakdown.push({
-            type: translation.chartPublicAvailable,
-            balance: spendable_bal / COIN,
-            colour: '#C898F5',
-        });
-    }
+            // Shielded (Available spendable)
+            const shield_spendable = activeWallet.shieldBalance;
+            if (shield_spendable > 0) {
+                res.push({
+                    type: `Shield Available-${vault.label}-${i}`,
+                    balance: shield_spendable / COIN,
+                    colour: baseColour.darken(0.2).toString(),
+                });
+            }
 
-    // Shielded (Available spendable)
-    const shield_spendable = activeWallet.shieldBalance;
-    if (shield_spendable > 0) {
-        arrBreakdown.push({
-            type: 'Shield Available',
-            balance: shield_spendable / COIN,
-            colour: '#9131EA',
-        });
-    }
+            // Shielded (Pending i.e still unspendable)
+            const shield_pending = activeWallet.pendingShieldBalance;
+            if (shield_pending > 0) {
+                res.push({
+                    type: `Shield Pending-${vault.label}-${i}`,
+                    balance: shield_pending1 / COIN,
+                    colour: baseColour.darken(0.1).toString(),
+                });
+            }
 
-    // Shielded (Pending i.e still unspendable)
-    const shield_pending = activeWallet.pendingShieldBalance;
-    if (shield_pending > 0) {
-        arrBreakdown.push({
-            type: 'Shield Pending',
-            balance: shield_pending / COIN,
-            colour: '#5e169c',
-        });
-    }
+            const immature_bal = activeWallet.immatureBalance;
+            if (immature_bal > 0) {
+                res.push({
+                    type: `${translation.chartImmatureBalance}-${vault.label}-${i}`,
+                    balance: immature_bal / COIN,
+                    colour: baseColour.lighten(0.1).toString(),
+                });
+            }
+            // Staking (Locked)
+            const spendable_cold_bal = activeWallet.coldBalance;
+            if (spendable_cold_bal > 0) {
+                res.push({
+                    type: `Staking-${vault.label}-${i}`,
+                    balance: spendable_cold_bal / COIN,
+                    colour: baseColour.darken(0.3).toString(),
+                });
+            }
 
-    const immature_bal = activeWallet.immatureBalance;
-    if (immature_bal > 0) {
-        arrBreakdown.push({
-            type: translation.chartImmatureBalance,
-            balance: immature_bal / COIN,
-            colour: '#4A1399',
-        });
-    }
-    // Staking (Locked)
-    const spendable_cold_bal = activeWallet.coldBalance;
-    if (spendable_cold_bal > 0) {
-        arrBreakdown.push({
-            type: 'Staking',
-            balance: spendable_cold_bal / COIN,
-            colour: '#721DEA',
-        });
-    }
+            const masternodes = await (
+                await Database.getInstance()
+            ).getMasternodes();
+            let masternodeValue = 0;
 
-    const masternodes = await (await Database.getInstance()).getMasternodes();
-    let masternodeValue = 0;
-
-    // Masternode (Locked)
-    for (const masternode of masternodes) {
-        if (
-            activeWallet.isCoinLocked(
-                new COutpoint({
-                    txid: masternode.collateralTxId,
-                    n: masternode.outidx,
-                })
-            )
-        ) {
-            masternodeValue += cChainParams.current.collateralInSats / COIN;
+            // Masternode (Locked)
+            for (const masternode of masternodes) {
+                if (
+                    activeWallet.isCoinLocked(
+                        new COutpoint({
+                            txid: masternode.collateralTxId,
+                            n: masternode.outidx,
+                        })
+                    )
+                ) {
+                    masternodeValue +=
+                        cChainParams.current.collateralInSats / COIN;
+                }
+            }
+            if (masternodeValue !== 0)
+                res.push({
+                    type: `Masternodes-${vault.label}-${i}`,
+                    balance: masternodeValue,
+                    colour: baseColour.darken(0.4).toString(),
+                });
         }
     }
-    if (masternodeValue !== 0)
-        arrBreakdown.push({
-            type: 'Masternodes',
-            balance: masternodeValue,
-            colour: 'rgba(19, 13, 30, 1)',
-        });
-    return arrBreakdown;
+    arrBreakdown.value = res;
 }
 
 /**
@@ -133,10 +158,11 @@ async function generateWalletBreakdown(arrBreakdown) {
             beforeDraw: (chart) => {
                 const ctx = chart.ctx;
                 const { top, left, width, height } = chart.chartArea;
-                const x = left + width / 2 - (image.width - 30) / 2;
-                const y = top + height / 2 - (image.height - 30) / 2;
+                const imgSize = 100;
+                const x = left + width / 2 - imgSize / 2;
+                const y = top + height / 2 - imgSize / 2;
                 ctx.globalAlpha = 1;
-                ctx.drawImage(image, x, y, image.width - 30, image.height - 30);
+                ctx.drawImage(image, x, y, imgSize, imgSize);
                 ctx.globalAlpha = 1;
             },
         };
@@ -183,21 +209,20 @@ async function generateWalletBreakdown(arrBreakdown) {
  * Render the wallet breakdown chart, or create it if not initialised
  */
 async function renderWalletBreakdown() {
-    // Update the chart data with the new dataset
-    const arrBreakdown = await getWalletDataset();
+    if (chartWalletBreakdown.value) {
+        // Update the chart
+        chartWalletBreakdown.value.data.labels = arrBreakdown.value.map(
+            (data) => data.type
+        );
 
-    // Update the chart
-    chartWalletBreakdown.value.data.labels = arrBreakdown.map(
-        (data) => data.type
-    );
-    chartWalletBreakdown.value.data.datasets[0].data = arrBreakdown.map(
-        (data) => data.balance
-    );
-    chartWalletBreakdown.value.data.datasets[0].backgroundColor =
-        arrBreakdown.map((data) => data.colour);
-    chartWalletBreakdown.value.update();
+        chartWalletBreakdown.value.data.datasets[0].data =
+            arrBreakdown.value.map((data) => data.balance);
+        chartWalletBreakdown.value.data.datasets[0].backgroundColor =
+            arrBreakdown.value.map((data) => data.colour);
+        chartWalletBreakdown.value.update();
+    }
 }
-
+const interval = ref();
 onMounted(async () => {
     Chart.register(
         Colors,
@@ -207,12 +232,19 @@ onMounted(async () => {
         Tooltip,
         LinearScale
     );
-
-    setInterval(async () => {
-        arrBreakdown.value = await getWalletDataset();
+    const updateData = async () => {
+        await getWalletDataset();
         await generateWalletBreakdown(arrBreakdown.value);
-        await renderWalletBreakdown();
-    }, 5000);
+    };
+    updateData();
+    interval.value = setInterval(updateData, 5000);
+});
+onUnmounted(() => {
+    clearInterval(interval.value);
+});
+
+watch(arrBreakdown, () => {
+    renderWalletBreakdown();
 });
 </script>
 
